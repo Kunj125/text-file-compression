3272c648fb814b59ee6fbb1f74419622e4ccedc3
w92120kd <kunj.dhola@student.manchester.ac.uk>
2023-09-01 15:16:10 +0530

    gets the frequeny of characters in the input string

--- HuffmanCompression.java
+++ HuffmanCompression.java
@@ -1,3 +1,18 @@
-public class HuffmanCompression{
-    
+import java.util.HashMap;
+import java.util.Map;
+
+public class HuffmanCompression {
+    public static Map<Character, Integer> huffmanTree(String text) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : text.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+        return frequencyMap;
+
+    }
+
+    public static void main(String[] args) {
+        String text = "Hello world";
+        System.out.println(huffmanTree(text));
+    }
 }

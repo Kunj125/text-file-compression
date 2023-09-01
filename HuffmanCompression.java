bcd3a971e507a0941332f7fc1d97f2dd207e105e
w92120kd <kunj.dhola@student.manchester.ac.uk>
2023-09-01 22:36:56 +0530

    finished implementing the huffman coding algorithm

--- HuffmanCompression.java
+++ HuffmanCompression.java
@@ -5,11 +5,10 @@ import java.util.PriorityQueue;
 public class HuffmanCompression {
     private static Map<Character, String> huffmanCodes = new HashMap<>();
     private static HuffmanNode root;
+    static Map<Character, Integer> frequencyMap = new HashMap<>();
 
     public static void huffmanTree(String text) {
         // gets the frequency of the characters
-        Map<Character, Integer> frequencyMap = new HashMap<>();
-        
         for (char c : text.toCharArray()) {
             frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
         }
@@ -48,9 +47,43 @@ public class HuffmanCompression {
         code.deleteCharAt(code.length() - 1);
     }
 
-    public static void main(String[] args) {
-        String text = "Hello world";
-        huffmanTree(text);
+    public static String compress(String text) {
+        StringBuilder compressedText = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            compressedText.append(huffmanCodes.get(c));
+        }
+        return compressedText.toString();
+    }
 
+    public static void main(String[] args) {
+        String text = "Hello World";
+        huffmanTree(text);
+        System.out.println(huffmanCodes);
+
+        String compressedText = compress(text);
+        System.out.println("Original text: " + text);
+        System.out.println("Compressed text: " + compressedText);
+        double originalSize = (text.length() * 8);
+        double compressedSize = compressedText.length();
+        double compressionRatio = originalSize / compressedSize;
+        System.out.println("Size of the original string: " + originalSize);
+        System.out.println("Size of the compressed string: " + compressedSize);
+        System.out.println("Compression ratio: " + compressionRatio);
+        // compression ratio
+        // int totalFrequency = 0;
+        // int freqCode = 0;
+        // for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+        // Character key = entry.getKey();
+        // Integer value = entry.getValue();
+        // totalFrequency += value;
+        // if (huffmanCodes.containsKey(key)) {
+        // freqCode += value * huffmanCodes.get(key).length();
+        // }
+        // }
+        // System.out.println(huffmanCodes);
+        // double compressedSize = freqCode + totalFrequency + (frequencyMap.size() *
+        // 8);
+        // System.out.println("Size of the compressed string: " + (compressedSize));
+        // System.out.println("Compression Ratio: " + originalSize/compressedSize);
     }
 }
